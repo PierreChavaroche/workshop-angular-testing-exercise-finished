0c7102c1f564334b03fd5b7bd7738e8a04c8dda0
Pierre Chavaroche <info@chavaroche.de>
2020-08-06 07:23:45 +0200

    First publish

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'workshop-angular-testing-exercise-finished'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('workshop-angular-testing-exercise-finished');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('workshop-angular-testing-exercise-finished app is running!');
-  });
 });
